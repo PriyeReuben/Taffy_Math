968a8e7db9c496076cb19cd188afe8acf8db1bfc
Priye Reuben <priyereuben93@gmail.com>
2018-01-19 23:26:29 -0500

    SImple GUI

--- logic.py
+++ logic.py
@@ -1,11 +1,11 @@
 import random as rand
 
-class Taffy():
+class Taffy:
     def __init__(self):
-        self.min = 4
-        self.max = 30
-        self.r1 = rand.randint(0, self.max - self.min) + self.min
-        self.r2 = rand.randint(0 , self.max - self.min) + self.min
+        self.min = 3 # int(input("Minimum Value?"))
+        self.max = 12 # int(input("Maximum Value?"))
+        #self.r1 = rand.randint(0, self.max - self.min) + self.min
+        #self.r2 = rand.randint(0 , self.max - self.min) + self.min
         rand.randint
         self.primes = [5,7,11]
 
@@ -16,32 +16,37 @@ class Taffy():
             seed1 = rand.randint(0,len(self.primes)-1)
             seed2 = rand.randint(0,len(self.primes)-1)
 
-            tValue = self.r1 * self.r2  #real value
-            fValue = int(((tValue + self.primes[seed1]) / 2 * 1.8 + self.primes[seed2]))
+            r1 = rand.randint(0, self.max - self.min) + self.min
+            r2 = rand.randint(0, self.max - self.min) + self.min
+
+
+            true_value = r1 * r2  #real value
+            false_value = int(((true_value + self.primes[seed1]) / 2 * 1.8 + self.primes[seed2]))
 
             given = rand.randint(1,2)
             if given==1:
-                gValue = tValue
+                gValue = true_value
             elif given==2:
-                gValue = fValue
+                gValue = false_value
             else:
                 print("Something went wrong")
 
-            expression = "{} * {} = {}".format(self.r1, self.r2, gValue)
+            expression = "{} * {} = {}".format(r1, r2, gValue)
             print(expression)
+            return(expression,true_value,false_value, gValue)
 
         elif sign==2:  #division to be fixed later
             seed1 = rand.randint(1, len(self.primes))
             seed2 = rand.randint(1, len(self.primes))
 
-            tValue = self.r1 * self.r2  # real value
-            fValue = int(((tValue + self.primes[seed1]) / 2 * 1.8 + self.primes[seed2]))
+            true_value = self.r1 * self.r2  # real value
+            false_value = int(((true_value + self.primes[seed1]) / 2 * 1.8 + self.primes[seed2]))
 
             given = rand.randint(1, 2)
             if given == 1:
-                gValue = tValue
+                gValue = true_value
             elif given == 2:
-                gValue = fValue
+                gValue = false_value
             else:
                 print("Something went wrong")
 
@@ -49,7 +54,6 @@ class Taffy():
             print(expression)
 
 def workhorse():
+    t = Taffy()
     for x in range(10):
-        t = Taffy()
         t.math(1)
-workhorse()
---
+++ testGUI.py
@@ -0,0 +1,59 @@
+import logic as taffy
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+
+correct_answers = 0
+incorrect_answers = 0
+
+
+
+window.title("T&F-fy Math")
+def setup():
+
+    taf = taffy.Taffy().math(1)
+
+    expression_label = ttk.Label(window, text=taf[0])
+    expression_label.grid(column=1, row=1)
+
+    results_label= ttk.Label(window, text = "Results")
+    results_label.grid(column=1, row =2)
+
+    #correct_answers_label = ttk.Label(window, text=correct_answers)
+    #correct_answers_label.grid(column =1, row =4)
+
+    #incorrect_answers_label = ttk.Label(window, text=incorrect_answers)
+    #incorrect_answers_label.grid(column=4, row=4)
+
+    def true_button_pressed():
+        expression_label.configure(text="")
+        if (taf[3]==taf[1]):
+            print("Correct")
+        elif(taf[3]==taf[2]):
+            print("Wrong")
+            print("correct answer is " + str(taf[1]))
+        else:
+            print(taf[3])
+        print("----------------------")
+        setup()
+    def false_button_pressed():
+        expression_label.configure(text=" ")
+        if taf[3]==taf[1]:
+            print("Wrong")
+        elif(taf[3]==taf[2]):
+            print("Correct")
+        else:
+            print(taf[3])
+        print("----------------------")
+        setup()
+
+
+    true_button = ttk.Button(window, text = "True", command = true_button_pressed).grid(column = 1, row = 3)
+    false_button = ttk.Button(window, text = "False", command = false_button_pressed).grid(column = 4, row = 3)
+
+
+
+setup()
+
+window.mainloop()

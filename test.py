3877be949330a0562f447b8b9323536b6a63c602
Priye Reuben <priyereuben93@gmail.com>
2021-10-19 18:28:33 -0400

    new and old

---
+++ test.py
@@ -0,0 +1,9 @@
+from random import randint
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+correct = 0
+wrong = 0
+
